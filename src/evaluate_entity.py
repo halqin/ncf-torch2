e8ce1df4c4ad795e35819713649dff57deb55a81
haqin <gavin.qinhao@gmail.com>
2022-06-26 09:40:24 +0200

    numpy to cpu

--- src/evaluate_entity.py
+++ src/evaluate_entity.py
@@ -43,13 +43,13 @@ def metrics(cat_data, predictions, label, top_k, is_train, threshold=0.5):
     else:
         y_score, indices = torch.topk(predictions, top_k)
         recommends = torch.take(
-            cat_data[:, 1], indices).tolist()
-        gt_item = cat_data[0, 1].tolist()  # ground truth, item id
+            cat_data[:, 1], indices).cpu().numpy().tolist()
+        gt_item = cat_data[0, 1].cpu().numpy().tolist()  # ground truth, item id
         pred_list = predictions.tolist()
         HR_1batch = hit(gt_item, recommends)
         NDCG_1batch = ndcg(gt_item, recommends)
-        ROC_1batch = roc(label.numpy(), pred_list)
-        ROC_top1batch = roc(label.numpy()[indices], y_score.detach().numpy())
-        recall_1batch = recall(label.numpy()[indices], y_score.detach().numpy(), threshold)
-        precision_1batch = precision(label.numpy()[indices], y_score.detach().numpy(), threshold)
+        ROC_1batch = roc(label.cpu().numpy(), pred_list)
+        ROC_top1batch = roc(label[indices].cpu().numpy(), y_score.detach().cpu().numpy())
+        recall_1batch = recall(label[indices].cpu().cpu().numpy(), y_score.detach().cpu().numpy(), threshold)
+        precision_1batch = precision(label[indices].cpu().numpy(), y_score.detach().cpu().numpy(), threshold)
     return HR_1batch, NDCG_1batch, ROC_1batch, ROC_top1batch, recall_1batch, precision_1batch
